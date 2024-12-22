f8cb5136fd70242b0716356f34536c6a070cd8cd
VeryrrDefine <1308061599@qq.com>
2024-12-22 15:35:10 +0800

    switch

--- app/src/main/java/org/yrjg/MainActivity.java
+++ app/src/main/java/org/yrjg/MainActivity.java
@@ -1,21 +1,37 @@
 package org.yrjg;
 
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView1;
+    private TextView textView2;
+    private Button button1;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch switch1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView1 = findViewById(R.id.textView);
-        // textView1.setText("你怎么这么自私，呸！");
+        textView1 = findViewById(R.id.textView);
         textView1.setText("a");
-        TextView textView2 = findViewById(R.id.textView2);
-        //textView2.setText("异常！你的电脑被破坏了，重启后将无法进入系统，如果你需要修复，请在下一个页面扫码");
+        textView2 = findViewById(R.id.textView2);
         textView2.setText("b");
+
+        button1 = findViewById(R.id.button);
+        switch1 = findViewById(R.id.switch1);
+        button1.setOnClickListener(view -> Toast.makeText(
+                MainActivity.this,
+                switch1.isChecked() ? "你好" : "好你",
+                Toast.LENGTH_SHORT).show()
+        );
     }
 }
